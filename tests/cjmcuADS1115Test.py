1b0f44746a2e28a13fe29a0682932554cb31d8f9
Elam Day <elam.day.friedland@gmail.com>
2021-01-19 12:12:01 -0800

    making tests

---
+++ tests/cjmcuADS1115Test.py
@@ -0,0 +1,24 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import math
+
+
+val = 1
+try:
+    # Create the I2C bus
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # Create the ADC object using the I2C bus
+    ads = ADS.ADS1115(i2c) # default addr; 48
+
+    # Create single-ended input on channels 0-2
+    chan0 = AnalogIn(ads, ADS.P0)
+    chan1 = AnalogIn(ads, ADS.P1)
+    chan2 = AnalogIn(ads, ADS.P2)
+    val = 0
+except:
+    pass
+print(val)

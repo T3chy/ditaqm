229c7b37d4474d604b0f1e9f140a44c37eb0123d
Elam Day <elam.day.friedland@gmail.com>
2021-01-21 10:47:08 -0800

    better exception handling:

--- postaqi.py
+++ postaqi.py
@@ -25,26 +25,35 @@ PWM_GPIO = 4 # physical port 7
 
 try: # TODO maybe the sensor bit vals should be int()ed
     # initalize sensors, host, name from config file
-    CONFIG = open("config", "r")
-    HOST = CONFIG.readline().strip("\n") + "/in"
-    NAME = CONFIG.readline().strip("\n")
-    BME = CONFIG.readline().strip("\n")
-    CJMCU= CONFIG.readline().strip("\n")
-    MHZ19B= CONFIG.readline().strip("\n")
-except Exception as error: # config file probably not created if this fails
+    with open("config", "r",) as CONFIG:
+        HOST = CONFIG.readline().strip("\n") + "/in"
+        NAME = CONFIG.readline().strip("\n")
+        BME = CONFIG.readline().strip("\n")
+        CJMCU= CONFIG.readline().strip("\n")
+        MHZ19B= CONFIG.readline().strip("\n")
+except IOError as error: # config file probably not created if this fails
+    print("config file has likley not been created! Please run \"sudo ./setup.sh\"")
+    print("error trace below:")
     print(error)
 
 if BME:
     try:
         i2c = I2C(board.SCL, board.SDA)
         bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-    except:
+        bme280.sea_level_pressure = 1013.25
+
+    except OSError:
+        print(1)
+    except ValueError:
         try:
             i2c = I2C(board.SCL, board.SDA)
             bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
-        except:
+            bme280.sea_level_pressure = 1013.25
+        except ValueError:
             print(1)
-    bme280.sea_level_pressure = 1013.25
+        except Exception as e:
+            print(e)
+
 if CJMCU:
     try:
         # Create the I2C bus
@@ -57,9 +66,14 @@ if CJMCU:
         chan0 = AnalogIn(ads, ADS.P0)
         chan1 = AnalogIn(ads, ADS.P1)
         chan2 = AnalogIn(ads, ADS.P2)
-    except:
+    except ValueError:
         print(1)
+    except OSError:
+        print(1)
+    except Exception as e:
+        print(e)
 
+# no setup required for mhz19b
 
 def update():
 
@@ -70,6 +84,7 @@ def update():
         res["humidity"] = bme280.humidity
         res["pressure"] = bme280.pressure
         res["altitude"] = bme280.altitude
+
     if CJMCU:
         ch0_oxy_r =  69./((5.0/chan0.voltage)-1.)
         ch1_nh3_r =  220./((5.0/chan1.voltage)-1.)
@@ -82,6 +97,7 @@ def update():
         res["NO2"] = 0.1516*math.pow(ch0_oxy_rratio, 0.9979)
         res["NH3"] = 0.6151*math.pow(ch1_nh3_rratio, -1.903)
         res["CO"] = 4.4638*math.pow(ch2_red_rratio, -1.177)
+
     if MHZ19B:
         raspi = pigpio.pi()
         pulse = read_PWM.reader(raspi, PWM_GPIO)

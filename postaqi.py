bc6192111d0077d75561887b4245b7cab18e7fee
Elam Day <elam.day.friedland@gmail.com>
2021-01-20 22:26:10 -0800

    hopefully more PEP-8-y

--- postaqi.py
+++ postaqi.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# TODO maybe recheck sensor connectedness at boot
 """Periodically send bme280 data via POST request to a designated host"""
 import sys
 import time
+import math
 import board
 import requests
 from busio import I2C
@@ -9,33 +11,33 @@ import adafruit_bme280
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-import math
 import pigpio
 sys.path.append("tests/")
 import read_PWM
 
 # constants
-R00 = 48.
-R01 = 29.
-R02 = 160.
+# resistance -> ppm constants (in zero air)
+OXIDATION_R0 = 48. # kΩ
+NH3_R0 = 29. # kΩ
+REDUCTION_R0 = 160. # kΩ
 
-PWM_GPIO = 4
-# Create library object using our Bus I2C port
-try:
-    config = open("config", "r")
-    host = config.readline().strip("\n") + "/in"
-    name = config.readline().strip("\n")
-    bme = config.readline().strip("\n")
-    cjmcu = config.readline().strip("\n")
-    mhz19b = config.readline().strip("\n")
-except Exception as e:
-    print(e)
+PWM_GPIO = 4 # physical port 7
 
-if bme:
+try: # TODO maybe the sensor bit vals should be int()ed
+    # initalize sensors, host, name from config file
+    CONFIG = open("config", "r")
+    HOST = CONFIG.readline().strip("\n") + "/in"
+    NAME = CONFIG.readline().strip("\n")
+    BME = CONFIG.readline().strip("\n")
+    CJMCU= CONFIG.readline().strip("\n")
+    MHZ19B= CONFIG.readline().strip("\n")
+except Exception as error: # config file probably not created if this fails
+    print(error)
+
+if BME:
     try:
         i2c = I2C(board.SCL, board.SDA)
         bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-        val = 0
     except:
         try:
             i2c = I2C(board.SCL, board.SDA)
@@ -43,7 +45,7 @@ if bme:
         except:
             print(1)
     bme280.sea_level_pressure = 1013.25
-if cjmcu:
+if CJMCU:
     try:
         # Create the I2C bus
         i2c = busio.I2C(board.SCL, board.SDA)
@@ -60,44 +62,45 @@ if cjmcu:
 
 
 def update():
+
     """gather data for connected sensors and return dictionary with it for POSTing"""
     res = {}
-    if bme:
+    if BME:
         res["temp"] = bme280.temperature
         res["humidity"] = bme280.humidity
         res["pressure"] = bme280.pressure
         res["altitude"] = bme280.altitude
-    if cjmcu:
-        ch0_oxy_R =  69./((5.0/chan0.voltage)-1.)
-        ch1_nh3_R =  220./((5.0/chan1.voltage)-1.)
-        ch2_red_R =  220./((5.0/chan2.voltage)-1.)
+    if CJMCU:
+        ch0_oxy_r =  69./((5.0/chan0.voltage)-1.)
+        ch1_nh3_r =  220./((5.0/chan1.voltage)-1.)
+        ch2_red_r =  220./((5.0/chan2.voltage)-1.)
 
-        ch0_oxy_Rratio = ch0_oxy_R/R00
-        ch1_nh3_Rratio = ch1_nh3_R/R01
-        ch2_red_Rratio = ch2_red_R/R02
+        ch0_oxy_rratio = ch0_oxy_r/OXIDATION_R0
+        ch1_nh3_rratio = ch1_nh3_r/NH3_R0
+        ch2_red_rratio = ch2_red_r/REDUCTION_R0
 
-        res["NO2"] = 0.1516*math.pow(ch0_oxy_Rratio, 0.9979)
-        res["NH3"] = 0.6151*math.pow(ch1_nh3_Rratio, -1.903)
-        res["CO"] = 4.4638*math.pow(ch2_red_Rratio, -1.177)
-    if mhz19b:
-        pi = pigpio.pi()
-        p = read_PWM.reader(pi, PWM_GPIO)
+        res["NO2"] = 0.1516*math.pow(ch0_oxy_rratio, 0.9979)
+        res["NH3"] = 0.6151*math.pow(ch1_nh3_rratio, -1.903)
+        res["CO"] = 4.4638*math.pow(ch2_red_rratio, -1.177)
+    if MHZ19B:
+        raspi = pigpio.pi()
+        pulse = read_PWM.reader(raspi, PWM_GPIO)
         time.sleep(5)
-        pp = p.pulse_period()
-        pw = p.pulse_width()
-        res["CO2"] = (5000.0)*(pw-2.0)/(pp-4.0)
-    res["name"] = name
+        pulse_period = pulse.pulse_period()
+        pulse_width = pulse.pulse_width()
+        res["CO2"] = (5000.0)*(pulse_width-2.0)/(pulse_period-4.0)
+    res["name"] = NAME
     return res
 
 if len(sys.argv) == 2:
     try:
-        print(requests.post(host, data=update()))
+        print(requests.post(HOST, data=update()))
     except Exception as e:
         print(e)
 else:
     while True:
         try:
-            print(requests.post(host, data=update()))
+            print(requests.post(HOST, data=update()))
         except Exception as e:
             print(e)
         time.sleep(55)
